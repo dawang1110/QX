78769aa6fa83bfbadf57befd3e3b945a5c2f8da3
dawang1110 <83797931+dawang1110@users.noreply.github.com>
2021-11-29 20:03:17 +0800

    Update jd_joy_feedPets.js

--- JD/jd_joy_feedPets.js
+++ JD/jd_joy_feedPets.js
@@ -7,18 +7,18 @@
 ==============Quantumult X==============
 [task_local]
 #京东宠汪汪喂食
-15 0-23/1 * * * https://raw.githubusercontent.com/dawang1110/QX/JD/jd_joy_feedPets.js, tag=京东宠汪汪喂食, img-url=https://raw.githubusercontent.com/58xinian/icon/master/jdcww.png, enabled=true
+15 0-23/1 * * * https://raw.githubusercontent.com/dawang1110/QX/main/JD/jd_joy_feedPets.js, tag=京东宠汪汪喂食, img-url=https://raw.githubusercontent.com/58xinian/icon/master/jdcww.png, enabled=true
 
 ==============Loon===============
 [Script]
-cron "15 0-23/1 * * *" script-path=https://raw.githubusercontent.com/dawang1110/QX/JD/jd_joy_feedPets.js,tag=京东宠汪汪喂食
+cron "15 0-23/1 * * *" script-path=https://raw.githubusercontent.com/dawang1110/QX/main/JD/jd_joy_feedPets.js,tag=京东宠汪汪喂食
 
 =========Surge=============
 [Script]
-京东宠汪汪喂食 = type=cron,cronexp="15 0-23/1 * * *",wake-system=1,timeout=3600,script-path=https://raw.githubusercontent.com/dawang1110/QX/JD/jd_joy_feedPets.js
+京东宠汪汪喂食 = type=cron,cronexp="15 0-23/1 * * *",wake-system=1,timeout=3600,script-path=https://raw.githubusercontent.com/dawang1110/QX/main/JD/jd_joy_feedPets.js
 
 ===============小火箭==========
-京东宠汪汪喂食 = type=cron,script-path=https://raw.githubusercontent.com/dawang1110/QX/JD/jd_joy_feedPets.js, cronexpr="15 0-23/1 * * *", timeout=3600, enable=true
+京东宠汪汪喂食 = type=cron,script-path=https://raw.githubusercontent.com/dawang1110/QX/main/JD/jd_joy_feedPets.js, cronexpr="15 0-23/1 * * *", timeout=3600, enable=true
 */
 //const zooFaker = require('./utils/JDJRValidator_Pure');
 //$.get = zooFaker.injectToRequest2($.get.bind($));
