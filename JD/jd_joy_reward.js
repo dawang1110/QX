25532d061b8b03a4e89b86f5b4f9a4cd8dd12a0e
dawang1110 <83797931+dawang1110@users.noreply.github.com>
2021-11-29 20:01:57 +0800

    Update jd_joy_reward.js

--- JD/jd_joy_reward.js
+++ JD/jd_joy_reward.js
@@ -9,17 +9,17 @@
 ==============Quantumult X==============
 [task_local]
 #宠汪汪积分兑换奖品
-59 7,15,23 * * * https://raw.githubusercontent.com/dawang/QX/JD/jd_joy_reward.js, tag=宠汪汪积分兑换奖品, img-url=https://raw.githubusercontent.com/58xinian/icon/master/jdcww.png, enabled=true
+59 7,15,23 * * * https://raw.githubusercontent.com/dawang1110/QX/main/JD/jd_joy_reward.js, tag=宠汪汪积分兑换奖品, img-url=https://raw.githubusercontent.com/58xinian/icon/master/jdcww.png, enabled=true
 
 ==============Loon==============
 [Script]
-cron "59 7,15,23 * * *" script-path=https:https://raw.githubusercontent.com/dawang/QX/JD/jd_joy_reward.js,tag=宠汪汪积分兑换奖品
+cron "59 7,15,23 * * *" script-path=https:https://raw.githubusercontent.com/dawang1110/QX/main/JD/jd_joy_reward.js,tag=宠汪汪积分兑换奖品
 
 ================Surge===============
-宠汪汪积分兑换奖品 = type=cron,cronexp="59 7,15,23 * * *",wake-system=1,timeout=3600,script-path=https://raw.githubusercontent.com/dawang/QX/JD/jd_joy_reward.js
+宠汪汪积分兑换奖品 = type=cron,cronexp="59 7,15,23 * * *",wake-system=1,timeout=3600,script-path=https://raw.githubusercontent.com/dawang1110/QX/main/JD/jd_joy_reward.js
 
 ===============小火箭==========
-宠汪汪积分兑换奖品 = type=cron,script-path=https://raw.githubusercontent.com/dawang/QX/JD/jd_joy_reward.js, cronexpr="59 7,15,23 * * *", timeout=3600, enable=true
+宠汪汪积分兑换奖品 = type=cron,script-path=https://raw.githubusercontent.com/dawang1110/QX/main/JD/jd_joy_reward.js, cronexpr="59 7,15,23 * * *", timeout=3600, enable=true
  */
 const $ = new Env('宠汪汪积分兑换奖品');
 // $.get = zooFaker.injectToRequest2($.get.bind($));
